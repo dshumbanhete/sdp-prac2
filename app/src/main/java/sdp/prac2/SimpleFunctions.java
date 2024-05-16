51377061f59e5b4025885515882615ed71b0bd42
Theresa Rautenbach <tessarautenbach@gmail.com>
2024-05-16 16:21:27 +0200

    Added isBracketMatched() to SimpleFunctions

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,19 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    public boolean isBracketMatched(String s) {
+        int openCount = 0, closeCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char curr = s.charAt(i);
+            if (curr == '(') {
+                openCount++;
+            } else if (curr == ')') {
+                closeCount++;
+            }
+        }
+        return openCount == closeCount;
+    }
 }
+
+
