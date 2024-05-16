0dc18d1e4a21e2eee5ddeb42fbe75bf6fa87cffb
Dean <admin@dhcp-b0b8.wifim.ru.ac.za>
2024-05-16 15:45:52 +0200

    Added functionality to add up values in list 'a', with indexes in 'b'. Task 1 g18s0463

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -1,7 +1,18 @@
 package sdp.prac2;
-
 import java.util.*;
+import java.util.ArrayList;
+
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
 }
+public int Task1(int[] a, int[] b){
+    int ans;
+    for (int i=0; i<b.length; i++){
+        if (b[i]<a.length){
+        ans+=a[b[i]];
+        }
+    }
+    return ans;
+}
+//method by g18s0463 used to add up the elements in a indexed by b
