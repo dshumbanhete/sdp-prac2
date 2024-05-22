3a80fccb001d50134dc9244d11aca5d8c892a039
tessagithub <89704334+tessagithub@users.noreply.github.com>
2024-05-22 15:27:33 +0200

    Task3 before merge to main

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,16 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
-}
+
+    public boolean Task3(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char curr = s.charAt(i);
+            if (curr == '(') {
+                stack.push(curr);
+            } else if (curr == ')' && !stack.isEmpty()) {
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
